300baf9bce6fe2ad9cbe0d392d01e8ae627205dd
kinasch <janik@schoenberg.name>
2022-12-13 04:05:30 +0100

    Day7: 1st star solution added

--- day7/main.js
+++ day7/main.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 function readFromFile(){
     try {
         let iPath = "./day7/input", tPath = "./day7/test/test1.txt";
-        let data = fs.readFileSync(tPath).toString();
+        let data = fs.readFileSync(iPath).toString();
         if(data === null) return;
         return data.split(/\r?\n/);
     } catch (err) {
@@ -16,31 +16,23 @@ let data = readFromFile();
 // if the lines does not start with a $, then check if it's already in the folder it's in currently
 // lines with ls can be ignored, lines with change current folder
 let system = {}; // basically '/'
-let currentFolder = system;
 let history = [];
 const navigator = (obj, path) => path.reduce((a, b) => a && a[b], obj);
 for (let i = 0; i < data.length; i++) {
     if(data[i].includes("$")) {
         if(data[i].includes(" cd")){
-            if(data[i].slice(5) === "..") {
-                history.pop();
-                // macht kein sinn aber hat funktioniert??
-                currentFolder = history.length > 0 ? navigator(system,history) : system;
-            }
-            else if(data[i].slice(5) === "/") currentFolder = system;
-            else {
-                history.push(data[i].slice(5));
-                currentFolder = currentFolder[data[i].slice(5)];
-            }
+            if(data[i].slice(5) === "..") history.pop();
+            else if(data[i].slice(5) === "/") {}
+            else history.push(data[i].slice(5));
         }
     }
     else {
         if(data[i].includes("dir")){
-            if(!currentFolder.hasOwnProperty(data[i].slice(4))) currentFolder[data[i].slice(4)] = {};
+            if(!navigator(system,history).hasOwnProperty(data[i].slice(4))) navigator(system,history)[data[i].slice(4)] = {};
         }
         else{ // files
             let file = data[i].split(' ');
-            currentFolder[file[1]] = parseInt(file[0]);
+            navigator(system,history)[file[1]] = parseInt(file[0]);
         }
     }
 }
@@ -52,8 +44,9 @@ function rec(systemObject,folder){
         if (typeof sumObject[folder] !== "number") sumObject[folder] = 0;
 
         if(typeof systemObject[key] === "object") {
-            rec(systemObject[key],key);
-            sumObject[folder] += sumObject[key];
+            // Add complete hiearchy to avoid problems with the same name of some folders
+            rec(systemObject[key],folder+","+key);
+            sumObject[folder] += sumObject[folder+","+key];
         } else {
             sumObject[folder] += systemObject[key];
         }
