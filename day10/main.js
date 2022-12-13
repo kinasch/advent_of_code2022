dd62433bf678f82f8118121e027a7e244640c42f
kinasch <janik@schoenberg.name>
2022-12-13 03:11:34 +0100

    Day10: 2nd star solution added

--- day10/main.js
+++ day10/main.js
@@ -28,3 +28,14 @@ for (let i = 0; i < data.length; i++) {
 }
 let sum = cycles[19]*20+cycles[59]*60+cycles[99]*100+cycles[139]*140+cycles[179]*180+cycles[219]*220;
 console.log(sum);
+
+let monitor="";
+for (let x = 0; x < 6; x++) {
+    for (let y = 0; y < 40; y++) {
+        let current = cycles.shift();
+        if(y<=current+1&&y>=current-1) monitor+="#";
+        else monitor+="."
+    }
+    monitor+="\n";
+}
+console.log(monitor);
